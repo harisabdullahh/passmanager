20e33e0fa81f7fece12e765df6d56d48321523d7
harisabdullahh <46281543+harisabdullahh@users.noreply.github.com>
2021-07-23 20:36:48 +0500

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.passwordmanager;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.sql.DatabaseMetaData;
+import java.util.concurrent.Executor;
+
+public class MainActivity extends AppCompatActivity {
+
+    databaseHelper myDB;
+    Button btnAdd, btnView;
+    EditText editText,editWebsite;
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnView = (Button) findViewById(R.id.btnView);
+
+        myDB = new databaseHelper(this);
+
+        executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Intent intent = new Intent(MainActivity.this, ViewListContents.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+            }
+        });
+
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric Authentication")
+                .setSubtitle("Login using fingerprint")
+                .setNegativeButtonText("Cancel")
+                .build();
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newEntry = editText.getText().toString();
+                if(editText.length() != 0 ){
+                    AddData(newEntry);
+                    editText.setText("");
+                }else{
+                    Toast.makeText(MainActivity.this, "No text found",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                biometricPrompt.authenticate(promptInfo);
+
+            }
+        });
+    }
+
+    public void AddData (String newEntry){
+        boolean insertData = myDB.addData(newEntry);
+
+        if(insertData==true){
+            Toast.makeText(MainActivity.this, "Successfully entered into DB!", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(MainActivity.this, "Error entering data into DB", Toast.LENGTH_LONG).show();
+        }
+    }
+}
